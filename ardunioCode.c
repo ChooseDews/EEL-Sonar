46e0c2289e7d04a443e5a031db17c8b64e389082
John Dews <John@thedews.us>
2019-12-04 17:16:23 -0500

    Update ardunioCode.c

--- ardunioCode.c
+++ ardunioCode.c
@@ -1,4 +1,3 @@
-
 #include <NewPing.h>
 #include <Servo.h>
 
@@ -8,13 +7,11 @@
 #define TRIGGER_PIN_2  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN_2     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+int pos = 0;    // variable to store the servo position
 
 Servo myservo;  // create servo object to control a servo
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonarTwo(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-
-int pos = 0;    // variable to store the servo position
-
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
@@ -24,9 +21,7 @@ void setup() {
 
 }
 
-
 void update(){
-
   int sonarOne = sonar.ping_cm();
   int sonarTwo = sonar.ping_cm();  
   Serial.print("");
@@ -35,46 +30,28 @@ void update(){
   Serial.print(sonarTwo); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.print(";");
   Serial.println(pos);
-  
   if(sonarOne == 0 || sonarOne > 30){
     digitalWrite(12, LOW);
   }else{
     digitalWrite(12, HIGH);
   }
-  
   if(sonarTwo == 0 || sonarTwo > 30){
     digitalWrite(2, LOW);
   }else{
     digitalWrite(2, HIGH);
   }
-
 }
 
 void loop() {
-
-
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    
-    myservo.write(pos);   
+  myservo.write(pos);   
   delay(40); // tell servo to go to position in variable 'pos'
-  update();
-    
-    }
+  update(); 
+  }
 
-    
   for (pos = 180; pos >= 0; pos -= 1){
-    
-    
   myservo.write(pos);   
   delay(40);
   update();
-  
-  
   }
-
-
-
-
-
-  
 }
